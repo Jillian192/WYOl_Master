b41ac6e8d0cc688a032fc08859855f7cc33ab3e7
dj <123456>
2019-07-06 09:31:17 +0800

    PathMeasure 路径长度

--- app/src/test/java/com/example/administrator/wyol_master/ExampleUnitTest.java
+++ app/src/test/java/com/example/administrator/wyol_master/ExampleUnitTest.java
@@ -2,7 +2,10 @@ package com.example.administrator.wyol_master;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +16,17 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void rex() throws Exception {
+        String info = "pjmfdsa54f5dsa3re...564";
+        String regex = "[^0-9.]";
+        Pattern pat = Pattern.compile(regex);
+        Matcher mat = pat.matcher(info);
+
+        System.out.println("☆☆☆:" + mat.replaceAll(""));
+
+
     }
 }
--- ui_master/src/main/java/com/example/ui_master/paint/PathMeasureView.java
+++ ui_master/src/main/java/com/example/ui_master/paint/PathMeasureView.java
@@ -1,13 +1,24 @@
 package com.example.ui_master.paint;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
  * Created by jieding on 2019/7/5.
  */
 public class PathMeasureView extends View {
+    private String TAG="☆PathMeasureView☆:";
+
+    private Paint mPaint;
+    private Paint mHolePaint;
+
     public PathMeasureView(Context context) {
         this(context,null);
     }
@@ -18,5 +29,43 @@ public class PathMeasureView extends View {
 
     public PathMeasureView(Context context,  AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(8);
+        mPaint.setColor(Color.RED);
+
+        mHolePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mHolePaint.setStyle(Paint.Style.STROKE);
+        mHolePaint.setColor(Color.BLUE);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawLine(0,getHeight()/2,getWidth(),getHeight()/2,mHolePaint);
+
+        canvas.drawLine(getWidth()/2,0,getWidth()/2,getHeight(),mHolePaint);
+       canvas.translate(getWidth()/2,getHeight()/2);
+
+        Path path = new Path();
+        path.lineTo(0,200);
+        path.lineTo(200,200);
+        path.lineTo(200,0);
+//        canvas.drawPath(path, mPaint);
+
+        PathMeasure pathMeasure = new PathMeasure();
+        pathMeasure.setPath(path,true);
+        Log.e(TAG, "onDraw:forceclose==true: "+ pathMeasure.getLength());
+        PathMeasure pathMeasure1 = new PathMeasure();
+        pathMeasure.setPath(path,false);
+        Log.e(TAG, "onDraw: forceclose==false:"+ pathMeasure1.getLength());
+        canvas.drawPath(path, mPaint);
     }
 }
